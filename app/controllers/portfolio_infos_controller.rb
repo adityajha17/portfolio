b3b8e98882ae738df80f5935b060bb65d6f687cd
Aditya jha <aditya.kumar@inverv.com>
2021-05-30 20:48:39 +0530

    Integrated new and create actions for portfolio_info

--- app/controllers/portfolio_infos_controller.rb
+++ app/controllers/portfolio_infos_controller.rb
@@ -2,4 +2,25 @@ class PortfolioInfosController < ApplicationController
     def index
         @portfolio_infos = PortfolioInfo.all
     end
+    def new
+        @portfolio_info = PortfolioInfo.new
+    end
+    def create
+        @portfolio_info = PortfolioInfo.new(portfolio_params)
+        respond_to do |format|
+            if @portfolio_info.save
+                format.html { redirect_to portfolio_infos_path, notice: 'Your portfolio is now live' }
+            else
+                format.html { render :new }
+            end
+        end
+    end
+
+    private
+
+    def portfolio_params
+        params.require(:portfolio_info).permit(:title, :subtitle, :body)
+    end
+
+
 end
